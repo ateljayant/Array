e0b56925d1e6d35429b1a3ad94b8072df0dafe8e
ateljayant <ateljayant@gmail.com>
2022-08-13 12:21:00 +0530

    yesyes

---
+++ increament.js
@@ -0,0 +1,8 @@
+const Inc_Arr = (array,N) => 
+{
+  let arr = "";
+  for(let i = 0; i < N; i++) {
+    arr += (array[i] + 1) + " ";
+  }
+   console.log(arr)
+};
---
+++ minMax.js
@@ -0,0 +1,14 @@
+function arrayMin(arr) {
+
+    
+    return Math.min(...arr);
+    
+  
+  };
+  
+  function arrayMax(arr) {
+  
+    
+    return Math.max(...arr);
+  
+  };
